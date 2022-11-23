383c33782013bed67224dde2035fff0c7a4a39fa
Rupali Anap <anaprupali333@gmail.com>
2022-11-23 09:52:16 -0800

    Added

---
+++ src/LinkedList/SimpleLinkList.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+public class SimpleLinkList {
+    public static void display() {
+
+        System.out.println("Welcome to Link List");
+    }
+
+    public static void main(String[] args) {
+
+        display();
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
+
+
+
